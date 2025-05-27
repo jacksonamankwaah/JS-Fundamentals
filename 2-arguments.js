41f9030c879962bcd20f57e8b5404169d04f2240
Amankwaah Jackson <amankwaahjackson@gmail.com>
2025-05-27 14:18:45 +0000

    args now defined

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,9 @@
-if (args.lenght === 0) {
+const args = process.argv.slice(2)
+
+if (args.length === 0) {
 console.log("No argument");
-} else if (args.lenght === 1) {
-console.log("Argument found")
+} else if (args.length === 1) {
+console.log("Argument found");
 } else {
 console.log("Arguments found");
 }
