ac10b43956a01b1a4517bc6fd7c7394d111effe0
Amankwaah Jackson <amankwaahjackson@gmail.com>
2025-05-28 16:18:45 +0000

    This script prints a factorial

---
+++ 10-factorial.js
@@ -0,0 +1,15 @@
+// This scrit prints a factorial
+
+function factorial(n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const number = parseInt(process.argv[2]);
+console.log(factorial(number));
+
